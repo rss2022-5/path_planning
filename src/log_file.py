37796f42db84311e6e390ecff8c8214ee0188469
Quinn Bowers <q.bowers@outlook.com>
2022-04-10 16:53:04 -0400

    some utils

---
+++ src/log_file.py
@@ -0,0 +1,28 @@
+
+class LogFile:
+    def __init__(self, filename, headers):
+        self.filename = filename
+        self.headers = headers
+        with open(self.filename, "w") as file:
+            file.write("time")
+            for header in headers:
+                file.write(",")
+                file.write(header)
+
+            file.write('\n')
+
+    def log(self, time, columns):
+        try:
+            assert(len(columns) == len(self.headers))
+        except AssertionError:
+            rospy.logwarn("Attempting to log an inconsistant number of values")
+            return
+
+        with open(self.filename, "a") as file:
+            file.write(time)
+            for col in columns:
+                file.write(",")
+                file.write(col)
+
+            file.write('\n')
+
