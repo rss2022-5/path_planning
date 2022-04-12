8a84b6b19e7081548b0ea4fed8a15d09104eef5f
Dizzy <q.bowers@outlook.com>
2022-04-12 18:27:38 -0400

    destination pubber hotfix

--- src/destination_pub.py
+++ src/destination_pub.py
@@ -2,24 +2,24 @@
 
 import sys
 import rospy
-from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import PoseStamped
 from tf.transformations import quaternion_from_euler
 
 if __name__ == "__main__":
-    rospy.init_node("initialpose_pub")
+    rospy.init_node("destination_pub")
     x = float(sys.argv[1])
     y = float(sys.argv[2])
     theta = float(sys.argv[3])
 
-    pub = rospy.Publisher("/move_base_simple/goal", PoseWithCovarianceStamped, queue_size=1, latch=True)
-    msg = PoseWithCovarianceStamped()
-    msg.pose.pose.position.x = x
-    msg.pose.pose.position.y = y
+    pub = rospy.Publisher("/move_base_simple/goal", PoseStamped, queue_size=1, latch=True)
+    msg = PoseStamped()
+    msg.pose.position.x = x
+    msg.pose.position.y = y
     o = quaternion_from_euler(0,0,theta)
-    msg.pose.pose.orientation.x = o[0]
-    msg.pose.pose.orientation.y = o[1]
-    msg.pose.pose.orientation.z = o[2]
-    msg.pose.pose.orientation.w = o[3]
+    msg.pose.orientation.x = o[0]
+    msg.pose.orientation.y = o[1]
+    msg.pose.orientation.z = o[2]
+    msg.pose.orientation.w = o[3]
 
     pub.publish(msg)
     rospy.sleep(10)
